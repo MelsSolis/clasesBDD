ba3207f271b9160efb7c7f6f1ab1a56974845c6a
Melmelada <breenhsolis@gmail.com>
2022-03-27 19:03:33 -0600

    Servidor creado

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
-
+from routes.user import user
 app = FastAPI()
+
+@app.get('/')
+def get_raiz():
+    return {"Hola aquí no hay nada, dirigete a /docs"}
+
+app.include_router(user)
+[15:43]
+############
+from fastapi import APIRouter
+
+user = APIRouter()
+
+@user.get('/users/')
+def get_users():
+    return {"Hola mundo"}
